a1d52211d854b5aa3a7dc26be96c8ccbba8ad484
Neosoft <navneet.neosoft@iiflw.com>
2022-08-14 22:10:06 +0530

    add news and faq function.

--- faq_management/views.py
+++ faq_management/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from .models import FAQ
+from .forms import FAQForm
+class FAQView(ListView):
+    model = FAQ
+    queryset = FAQ.objects.all()
+    template_name = 'faq_management/list_view.html'
+    context_object_name = 'faq_list'
 
-# Create your views here.
-def add_faq(request):
-    return "Hello From FAQ"
+class FAQDetailedView(DetailView):
+    model = FAQ
+    template_name = 'faq_management/detail_view.html'
+    context_object_name = 'faq_detail'
+
+class AddFAQView(CreateView):
+    model = FAQ
+    form_class = FAQForm
+    template_name = 'faq_management/add_faq.html'
+    success_url = '/faq_management'
+
+class EditFAQView(UpdateView):
+    model = FAQ
+    form_class = FAQForm
+    pk_url_kwarg = 'pk'
+    template_name = 'faq_management/edit_faq.html'
+    success_url = '/faq_management'
+
+class DeleteFAQView(DeleteView):
+    model = FAQ
+    pk_url_kwarg = 'pk'
+    template_name = 'faq_management/delete_view.html'
+    success_url = '/faq_management'
