a3287a734a847094ca40c9c12afacbfab9e5792b
Neosoft <navneet.neosoft@iiflw.com>
2022-08-14 16:56:25 +0530

    remove /

--- blog_management/urls.py
+++ blog_management/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     # path('add', views.add, name="add"),
     path('', BlogView.as_view(), name="blog_list"),
     path("<int:pk>", BlogDetailedView.as_view(), name="blog_detail_view"),
-    path('/add_blog', AddBlogView.as_view(), name="add_blog"),
-    path('/edit_blog/<int:pk>', EditBlogView.as_view(), name="edit_blog"),
-    path('/delete_blog/<int:pk>', DeleteBlogView.as_view(), name="delete_blog")
+    path('add_blog', AddBlogView.as_view(), name="add_blog"),
+    path('edit_blog/<int:pk>', EditBlogView.as_view(), name="edit_blog"),
+    path('delete_blog/<int:pk>', DeleteBlogView.as_view(), name="delete_blog")
     ]
 
 
