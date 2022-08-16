33bbae51eeda113271baffe2b6899abf1f38d6e3
Neosoft <navneet.neosoft@iiflw.com>
2022-08-16 10:44:58 +0530

    add authentication app

--- backend/settings.py
+++ backend/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
     'category_management',
     'blog_management',
     'news_management',
--- category_management/urls.py
+++ category_management/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('', CategoryView.as_view(), name="category_list"),
     path("<int:pk>", CategoryDetailedView.as_view(), name="category_detail_view"),
-    path('category_management/add_category', AddCategoryView.as_view(), name="add_category"),
-    path('category_management/edit_category/<int:pk>', EditCategoryView.as_view(), name="edit_category"),
-    path('category_management/delete_category/<int:pk>', DeleteCategoryView.as_view(), name="delete_category")
+    path('add_category', AddCategoryView.as_view(), name="add_category"),
+    path('edit_category/<int:pk>', EditCategoryView.as_view(), name="edit_category"),
+    path('delete_category/<int:pk>', DeleteCategoryView.as_view(), name="delete_category")
     ]
