544205b2a5fce76b5369833096bccc66d56cee01
Neosoft <navneet.neosoft@iiflw.com>
2022-08-18 23:49:15 +0530

    debug false

--- backend/settings.py
+++ backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f9h&cf%b2&$1a)iru8c+8)5-m3r2lt+u-8r2b$#)u&p%q^#$f0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
